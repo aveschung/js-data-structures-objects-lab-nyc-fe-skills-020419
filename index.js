4b5c688c3b189a686fea574c8fb20a75fbec0cd7
sagacious-flag-2805 <avery.chung@wework.com>
2019-03-19 00:33:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   return driverCopy
 }
 
-function updateDriverWithKeyAndValue(driver, key, value)
-  
+function updateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value
+  return driver
+}
