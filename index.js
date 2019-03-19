2bf8f55a4406ef849ffd29cb6260dbf716dbeacd
sagacious-flag-2805 <avery.chung@wework.com>
2019-03-19 00:32:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Write your solution in this file!
 const driver = {}
 
-function 
+function updateDriverWithKeyAndValue(driver, key, value) {
+  let driverCopy = {...driver}
+  driverCopy[key] = value 
+  return driverCopy
+  
+}
